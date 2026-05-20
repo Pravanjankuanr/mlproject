fff1b5087a071fdb4436f3036c59dd7a10b8da74
Pravanjan_Kuanr <pravanjankuanr2002@gmail.com>
2026-05-21 01:06:48 +0530

    Added setup.py, requirements.txt, and src package structure

---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import find_packages, setup
+from typing import List
+
+hypen_e_dot = '-e .'
+
+def get_requirements(file_path:str) -> List[str]:
+    requirements = []
+    with open(file_path) as f:
+        requirements = f.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if hypen_e_dot in requirements:
+            requirements.remove(hypen_e_dot)
+            
+    return requirements
+
+setup(
+    name="ml_project",
+    version="0.0.1",
+    author="Pravanjan",
+    author_email="pravanjan2002@gmail.com",
+    packages=find_packages(),
+    install_requires=get_requirements('requirements.txt')
+)
